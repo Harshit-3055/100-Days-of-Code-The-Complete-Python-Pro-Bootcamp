a90a5beae74887771712fe167783b1a019b966db
Harshit-3055 <harshittag@icloud.com>
2024-03-17 22:43:36 +0530

    Completed the Logic

--- Day-9/Day-9(Auction).py
+++ Day-9/Day-9(Auction).py
@@ -4,8 +4,13 @@ anyotherbidder = "yes"
 while anyotherbidder=="yes":
     name = input("What is your name?: ")
     bid = int(input("What's your bid?: $"))
-    bidders.update({name:bid})
+    bidders.update({bid:name})
     anyotherbidder = input("Are there any other bidders? Type 'yes' or 'no'.")
 
 
-print(bidders)
+allthevalues = bidders.keys()
+winneramt = max(allthevalues)
+winnername = bidders[winneramt]
+
+
+print("The winner of the bid is ",winnername,"with a bid of $",winneramt)
